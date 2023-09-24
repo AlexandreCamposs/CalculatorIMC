bba557282bd6476d1b454528b50ff12716a1b41c
AleCampos <alemdcamposs@gmail.com>
2023-09-24 18:20:16 -0300

    att

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,27 @@ import { BsHouse } from 'react-icons/bs';
 import { AiFillInfoCircle } from 'react-icons/ai';
 import Form from './components/Form';
 
-import ImageImc from '../public/IMC.jpg';
+import ImageImc from '/IMC.jpg';
+import { useState } from 'react';
 
 function App() {
+  const [user, setUser] = useState({});
+  const [dataValue, setDataValue] = useState({});
+  const [messageImc, setMessageImc] = useState('');
+
+  console.log(messageImc);
+
+  const userValue = (user) => {
+    setUser(user);
+  };
+
+  const valueImc = (data, message) => {
+    setDataValue(data);
+    setMessageImc(message);
+  };
+
+  console.log(user);
+  console.log(dataValue);
   return (
     <div className="app">
       <div className="container-calculator">
@@ -19,24 +37,53 @@ function App() {
             <AiFillInfoCircle />
           </div>
           <img src={ImageImc} alt="" />
-          <Form />
+          <Form user={userValue} valorIMC={valueImc} />
         </div>
       </div>
       <div className="container-result">
         <div>
-          <h2>Peso Normal</h2>
+          <h2>{messageImc}</h2>
         </div>
-        <div>
-          <p>Seu índice IMC é:</p>
-          {}
-          Peso ideal:
-          {}
+        <div className="data">
+          {messageImc && (
+            <>
+              {' '}
+              <p>{user.name} seu índice IMC é :</p>
+              <span>{dataValue.resultImc} kg/m²</span>
+              <p>Peso ideal:</p>
+              <span>
+                {dataValue.pesoMin} a kg {dataValue.pesoMax}
+              </span>
+            </>
+          )}
         </div>
 
-        <div></div>
+        <div className="container-graus">
+          <div class=" degrad">
+            <div class="box">
+              <span> &lt; 18,5</span>
+            </div>
+            <div class="box">
+              <span> 18,5 a 24,9</span>
+            </div>
+            <div class="box">
+              <span> 25 a 30</span>
+            </div>
+            <div class="box">
+              <span> &gt; 30</span>
+            </div>
+          </div>
+          <div class="data-box ">
+            <div class="box ">Magreza</div>
+            <div class="box ">Peso ideal</div>
+            <div class="box ">Sobrepeso</div>
+            <div class="box ">Obesidade</div>
+          </div>
+        </div>
       </div>
     </div>
   );
 }
 
 export default App;
+// handleInputChange
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,18 +1,96 @@
 import Button from './Button.jsx';
-
 import '../styles/components/_form.scss';
+import { useState } from 'react';
 
-const Form = () => {
+const Form = ({ user, valorIMC }) => {
+  const [name, setName] = useState('');
+  const [altura, setAltura] = useState('');
+  const [peso, setPeso] = useState('');
+
+  const handleInputChange = (e) => {
+    e.preventDefault();
+
+    const userData = { name, altura, peso };
+    calculatorIMC(userData.altura, userData.peso);
+    user(userData);
+    // resetInput();
+  };
+
+  const formatAltura = (value) => {
+    value = value.replace(/[,\.]/g, '');
+
+    if (value.length >= 2) {
+      const formattedValue =
+        value.substring(0, 1) + '.' + value.substring(1, 3);
+      setAltura(formattedValue);
+    } else if (value.length === 1) {
+      setAltura(value + '.');
+    } else {
+      setAltura(value);
+    }
+  };
+
+  const resetInput = () => {
+    setName('');
+    setAltura('');
+    setPeso('');
+  };
+
+  const calculatorIMC = (a, p) => {
+    const peso = parseFloat(p);
+    const altura = parseFloat(a);
+
+    const resultImc = (peso / (altura * altura)).toFixed(1);
+    const pesoMin = (18.6 * altura * altura).toFixed(1);
+    const pesoMax = (24.9 * altura * altura).toFixed(1);
+
+    const resultData = { pesoMin, pesoMax, resultImc };
+
+    if (resultImc < 18.5) {
+      valorIMC(resultData, 'Abaixo do normal');
+    } else if (resultData <= 24.9) {
+      valorIMC(resultData, 'Normal');
+    } else if (resultData <= 30) {
+      valorIMC(resultData, 'Sobrepeso');
+    } else {
+      valorIMC(resultData, 'Obesidade');
+    }
+  };
+
   return (
-    <form>
+    <form onSubmit={handleInputChange}>
+      <label>
+        Nome:
+        <input
+          type="text"
+          placeholder="ex: Alexandre"
+          required
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label>
       <div className="form">
         <label>
           Altura:
-          <input type="text" name="altura" placeholder="0,00 m" />
+          <input
+            type="text"
+            placeholder="M"
+            required
+            maxLength={4}
+            value={altura}
+            onChange={(e) => formatAltura(e.target.value)}
+          />
         </label>
         <label>
           Peso:
-          <input type="text" name="peso" placeholder="0,0 kg" />
+          <input
+            type="text"
+            placeholder="Kg"
+            required
+            maxLength={3}
+            value={peso}
+            onChange={(e) => setPeso(e.target.value)}
+          />
         </label>
       </div>
       <Button>Calcular</Button>
